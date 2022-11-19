a1dab73a650b53ab0889836c87c8a6ad2a1a4c46
Lokchand95 <lokchand.rs@gmail.com>
2022-11-19 13:05:41 +0000

    first commit

---
+++ src/main/java/com/Lokchand/CICD/Controller/CiCDController.java
@@ -0,0 +1,19 @@
+package com.Lokchand.CICD.Controller;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/user")
+public class CiCDController {
+
+    @GetMapping("/getCustomer")
+    public String getCustomerData(){
+        return "Hello welcome for CICD pipleine";
+    }
+
+}
